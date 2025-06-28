6e9c8927b7c7bff7c0ccb3df399ba97b8a91e580
Gerardo Valencia <valenciacartes.gerardo@gmail.com>
2025-06-28 16:12:44 -0400

    Reemplazar bcrypt por bcryptjs para compatibilidad con Render

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,5 @@
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const pool = require('../models/db');
 
 exports.register = async (req, res) => {
